6d9f89d5dbfdd3e774e3f0a0fccd82a029b843c8
mqn <214261550@qq.com>
2019-06-20 20:55:06 +0800

    '调整初始目录'

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,24 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div>
+
     </div>
-    <router-view/>
-  </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  props: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  created () {},
+  mounted () {},
+  watch: {},
+  methods: {},
+  components: {}
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+</script>
+
+<style scoped lang="less">
+
 </style>
