4abdcdd1bb6b955f55e85df7ab98c0e811d56536
mqn <214261550@qq.com>
2019-06-21 10:36:03 +0800

    '修改app‘

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,15 @@
 <template>
-    <div>
-
-    </div>
+  <div>
+  </div>
 </template>
-
 <script>
 export default {
   props: {},
   data () {
     return {}
-  },
-  computed: {},
-  created () {},
-  mounted () {},
-  watch: {},
-  methods: {},
-  components: {}
+  }
 }
+
 </script>
-
 <style scoped lang="less">
-
 </style>
