348187b0bec2f318db751fd66f8480a6a6604783
mqn <214261550@qq.com>
2019-06-22 14:12:37 +0800

    '添加home和login路由'

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,16 +1,19 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+  <div>login component</div>
 </template>
 <script>
 export default {
   props: {},
   data () {
     return {}
-  }
+  },
+  computed: {},
+  created () { },
+  mounted () { },
+  watch: {},
+  methods: {},
+  components: {}
 }
-
 </script>
 <style scoped lang="less">
 </style>
